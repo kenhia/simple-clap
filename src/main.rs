a89171a698f2475ef11c7bc33c5a9639395ff0eb
Ken Hiatt <ken.hiatt@gmail.com>
2024-05-28 13:17:43 -0700

    arg counting and flag

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
-use clap::{Parser, Subcommand};
+use clap::{ArgAction, Parser, Subcommand};
 
 #[derive(Parser)]
 #[command(version = "0.1.0", author = "Ken Hiatt", about = "playing with clap")]
 struct Opts {
-    #[clap(short, long, help = "verbose mode")]
-    verbose: bool,
+    #[clap(short, long, help = "verbose mode (repeat for more)", action = ArgAction::Count)]
+    verbose_level: u8,
+
+    #[clap(short, long, help = "Enable debug mode")]
+    debug: bool,
+
     #[clap(subcommand)]
     cmd: Commands,
 }
@@ -19,8 +23,13 @@ enum Commands {
 
 fn main() {
     let opts = Opts::parse();
-    if opts.verbose {
-        println!("Verbose mode enabled");
+    if opts.debug {
+        println!("Debug mode enabled");
+    }
+    match opts.verbose_level {
+        0 => println!("Verbose mode disabled"),
+        1 => println!("Verbose mode enabled"),
+        _ => println!("Verbose mode enabled (x{})", opts.verbose_level),
     }
     match opts.cmd {
         Commands::Info { device } => {
