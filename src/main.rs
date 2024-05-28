4038e5e5f250a05ec4803ce6ce8c84822163d8fa
Ken Hiatt <ken.hiatt@gmail.com>
2024-05-28 09:26:11 -0700

    switch to derive

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,17 @@
-use clap::{Command, Arg};
-use clap::ColorChoice;
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(
+    version = "0.1.0",
+    author = "Ken Hiatt",
+    about = "playing with clap",
+)]
+struct Opts {
+    #[clap(help="Device to query")]
+    device: String,
+}
 
 fn main() {
-    let matches = Command::new("lsblk")
-        .version("0.1.0")
-        .author("Ken Hiatt <ken.hiatt@gmail.com>")
-        .color(ColorChoice::Always)
-        .arg(
-            Arg::new("device")
-                .help("Device to query")
-                .required(true)
-                .index(1)
-        )
-        .get_matches();
-
-    if let Some(device) = matches.get_one::<String>("device") {
-        println!("Listing device: {}", device);
-    } else {
-        println!("Listing all devices");
-    }
+    let opts = Opts::parse();
+    println!("device: {}", opts.device);
 }
